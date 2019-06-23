deca8d4280345bc03607495eae85289d0ee02f36
whtirado <whtirado@gmail.com>
2019-06-22 23:58:11 -0400

    New changes

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist', 'online-resume')));
 
 app.get('/*', (req, res) => {
-  res.sendfile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendfile(path.join(__dirname, 'dist', 'online-resume', 'index.html'));
 });
 
 app.listen(port, () => {
