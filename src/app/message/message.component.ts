56707fbc2c99d275eacb841a84d8dd5cd3857ce6
whtirado <whtirado@gmail.com>
2019-07-02 14:15:00 -0400

    Added empty message element in messages component.

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 })
 @Injectable()
 export class MessageComponent implements OnInit {
-  public messages: IMessage[];
+  public messages: IMessage[] = [];
   public errorMessage = '';
 
   constructor(
